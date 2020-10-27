3a9efee2fb1790a6f911a50b03d388e6ea064461
Ayat <ayatalhamadcs@gmail.com>
2020-10-28 00:29:51 +0300

    End Part 3

--- ZwajAPI/Controllers/AuthController.cs
+++ ZwajAPI/Controllers/AuthController.cs
@@ -1,5 +1,11 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using ZwajAPI.Data;
 using ZwajAPI.Dtos;
 using ZwajAPI.Models;
@@ -11,10 +17,12 @@ namespace ZwajAPI.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IAuthRepository repo , IConfiguration  configuration)
         {
             _repo = repo;
-
+            _configuration = configuration;
         }
 
 
@@ -22,7 +30,6 @@ namespace ZwajAPI.Controllers
          public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
          {
 
-             var aoooo="gggg";
              userForRegisterDto.UserName = userForRegisterDto.UserName.ToLower() ;
              if(await _repo.UserExists(userForRegisterDto.UserName) ) 
              return BadRequest(" هذا المستخدم مستخدم من قبل ");
@@ -31,6 +38,33 @@ namespace ZwajAPI.Controllers
              return StatusCode(201) ; 
              
          }
+
+        [HttpPost("login")]
+         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto )
+         {
+             var userFromRep = await _repo.Login(userForLoginDto.username.ToLower() , userForLoginDto.password) ; 
+             if(userFromRep==null) return Unauthorized()  ; 
+              var claims = new [] {
+                  new Claim(ClaimTypes.NameIdentifier , userFromRep.Id.ToString()) ,
+                  new Claim(ClaimTypes.Name , userFromRep.UserName)
+
+              } ; 
+               var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value)) ;
+               
+               var cards = new SigningCredentials(key , SecurityAlgorithms.HmacSha512) ; 
+               var tokenDescripror  =  new SecurityTokenDescriptor{
+                   Subject = new ClaimsIdentity(claims)   , 
+                   Expires = DateTime.Now.AddDays(1) ,
+                   SigningCredentials = cards  
+                   
+               } ;
+               
+                var tokenHandler  =  new JwtSecurityTokenHandler();
+                var token = tokenHandler.CreateToken(tokenDescripror) ;
+                return Ok(new {token=tokenHandler.WriteToken(token)}) ;
+
+         }
+
 
 
     }
--- ZwajAPI/Controllers/ValuesController.cs
+++ ZwajAPI/Controllers/ValuesController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ZwajAPI.Data;
 
 namespace ZwajAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -30,10 +32,11 @@ namespace ZwajAPI.Controllers
         }  // GET api/values
         
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return "value 1";
         }
 
         // POST api/values
